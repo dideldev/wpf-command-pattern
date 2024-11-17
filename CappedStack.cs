1aace52748da62a321e6ceb2252fff8c28e29ac5
Diego Delgado <dieog.delgado.dev@gmail.com>
2024-11-17 11:36:37 +0100

    Add fixed count command manager

---
+++ CappedStack.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dideldev.Wpf.CommandPattern
+{
+    /// <summary>
+    /// Defines a stack with a limited size. 
+    /// </summary>
+    /// <remarks>
+    /// It is implemented through lists.
+    /// </remarks>
+    /// <typeparam name="T">Type of the content.</typeparam>
+    /// <remarks>
+    /// Initializes a new instance of <see cref="CappedStack{T}"/>.
+    /// </remarks>
+    public class CappedStack<T> : IEnumerable<T>
+    {
+        /// <summary>
+        /// Item container.
+        /// </summary>
+        private List<T> items = [];
+
+        /// <summary>
+        /// Maximum number of items in the stack. 
+        /// </summary>
+        public int MaxCount { get; private set; } = 1000;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="CappedStack{T}"/>.
+        /// </summary>
+        /// <param name="maxCount"></param>
+        public CappedStack(int maxCount = 1000)
+        {
+            this.MaxCount = maxCount;
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(maxCount);
+        }
+
+        /// <summary>
+        /// Get or sets the items in the stack.
+        /// </summary>
+        public List<T> Items
+        {
+            get => items;
+            internal set => items = value;
+        }
+
+        /// <summary>
+        /// Get or sets the numeber of items in the stack. 
+        /// </summary>
+        public int Count => items.Count;
+
+        /// <summary>
+        /// Gets wether the stack contains any item or not.
+        /// </summary>
+        public bool HasItems => items.Count > 0;
+
+
+        /// <summary>
+        /// Remove all elements in the stack. 
+        /// </summary>
+        public void Clear()
+        {
+            items.Clear();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)items).GetEnumerator();
+        }
+
+        /// <summary>
+        /// Gets the last added element on the stack and removeit. 
+        /// </summary>
+        /// <returns></returns>
+        public T Pop()
+        {
+            if (items.Count == 0)
+                throw new InvalidOperationException("Pop when no items left on the stack.");
+
+            T item = items[^1];
+            items.RemoveAt(items.Count - 1);
+            return item;
+        }
+
+        /// <summary>
+        /// Adds a new item on the top of the stack.
+        /// </summary>
+        /// <param name="item"></param>
+        public void Push(T item)
+        {
+            items.Add(item);
+            while (items.Count > MaxCount)
+            {
+                items.RemoveAt(0);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)items).GetEnumerator();
+        }
+    }
+}
---
+++ FixedCountCommandManager.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dideldev.Wpf.CommandPattern
+{
+    /// <summary>
+    /// Command manager that keeps a maximum number of commands on the stack.
+    /// </summary>
+    /// <remarks>
+    /// It is not guaranteed to occupy a limited amount of memory.
+    /// Commands may have lot of data to store. 
+    /// TransactionCommands may have several commands inside and still count as one.
+    /// </remarks>
+    /// <typeparam name="T">Type of the context where the commands operate.</typeparam>
+    /// <remarks>
+    /// Initializes a new instance of <see cref="FixedCountCommandManager{T}"/>.
+    /// </remarks>
+    /// <param name="maxCount"></param>
+    public class FixedCountCommandManager<T>(int maxCount = 1000) : ICommandManager<T>
+        where T : class
+    {
+        /// <summary>
+        /// Context where the commands opeartes and make changes.
+        /// </summary>
+        private T? context;
+
+        /// <summary>
+        /// Maximum number of commands stored on a stack.
+        /// </summary>
+        private readonly int maxCount = maxCount;
+
+        /// <summary>
+        /// Last exexuted commands. 
+        /// </summary>
+        protected CappedStack<Command<T>> executedCommands = new();
+
+        /// <summary>
+        /// Last undone commands. 
+        /// </summary>
+        protected CappedStack<Command<T>> undoneCommands = new();
+
+        /// <summary>
+        /// Gets or sets the context where de commands operates.
+        /// </summary>
+        public T? Context { get => context; set => context = value; }
+
+        /// <summary>
+        /// Perform the action of the given command and stores it so it can be undone.
+        /// </summary>
+        /// <param name="cmd"></param>
+        public void Do(Command<T> cmd)
+        {
+            cmd.Do(context);
+            executedCommands.Push(cmd);
+
+            if (undoneCommands.Count > 0)
+                undoneCommands.Clear();
+
+
+            if (this.executedCommands.Count > maxCount)
+            {
+                // The command is now lost and cannot be undo
+                this.executedCommands.Items.RemoveAt(0);
+            }
+        }
+
+        /// <summary>
+        /// Rollbacks the action done by the last executed command.
+        /// </summary>
+        /// <returns>The command that has been undone. It may have information relevant to the view model or the view.</returns>
+
+        public Command<T>? Undo()
+        {
+            if (executedCommands.Count > 0)
+            {
+                Command<T> cmd = executedCommands.Pop();
+                cmd.Undo(context);
+                undoneCommands.Push(cmd);
+                return cmd;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Do the action of the last undone command.
+        /// </summary>
+        /// <returns>The command that has been undone. It may have information relevant to the view model or the view.</returns>
+
+        public Command<T>? Redo()
+        {
+            if (undoneCommands.Count > 0)
+            {
+                Command<T> cmd = undoneCommands.Pop();
+                cmd.Do(context);
+                executedCommands.Push(cmd);
+                return cmd;
+            }
+            return null;
+        }
+    }
+}
